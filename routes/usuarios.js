b00bcd9d813d900890bc324f97d4ecda95020b04
Elvis Paico <elvispaico.2016@gmail.com>
2020-10-18 18:43:46 -0500

    Fin seccion 10 - Backend

---
+++ routes/usuarios.js
@@ -0,0 +1,16 @@
+/*
+    path: api/usuarios
+*/
+
+const { Router } = require('express');
+const { check } = require('express-validator');
+const { getUsuarios } = require('../controllers/usuarios');
+const { validarJWT } = require('../middlewares/validar-jwt');
+
+
+const router = Router();
+
+router.get('/', validarJWT, getUsuarios );
+
+
+module.exports = router;
--- sockets/socket.js
+++ sockets/socket.js
@@ -1,12 +1,33 @@
 const { io } = require('../index');
-
+const { comprobarJWT } = require('../helpers/jwt');
+const { usuarioConectado, usuarioDesconectado, grabarMensaje} = require('../controllers/socket');
 
 // Mensajes de Sockets
-io.on('connection', client => {
+io.on('connection', (client) => {
     console.log('Cliente conectado');
 
+    //obtiene el token
+    const [valido, uid] = comprobarJWT(client.handshake.headers['x-token']);
+
+    //verifica autenticacion
+    if( !valido ) { return client.disconnect();}
+    //Cliente autenticado
+    usuarioConectado(uid);
+
+    //ingresar al usuario a una sala en particular
+    //sala global, cliente.id, 5f81f8fb5a320c3ca443546c
+    client.join( uid );
+
+    //escuchar del cliente el mensaje personal
+    client.on('mensaje-personal', async (payload) => {
+        //grabar mensaje
+        await grabarMensaje( payload );
+        
+        io.to( payload.para ).emit('mensaje-personal', payload );
+    })
+
     client.on('disconnect', () => {
-        console.log('Cliente desconectado');
+        usuarioDesconectado(uid);
     });
 
     /*client.on('mensaje', ( payload ) => {
